31d3c9132d11ae8804c82c53abcea6c81ed11eae
Michael Bogale <127705286+workingbetter@users.noreply.github.com>
2024-09-09 20:04:54 +0100

    Add files via upload

---
+++ Tablelamp.py
@@ -0,0 +1,27 @@
+from gpiozero import LED, Button
+import time
+
+led = LED(17) # define LED pin according to BCM Numbering
+button = Button(18) # define Button pin according to BCM Numbering
+
+def onButtonPressed():  # When button is pressed, this function will be executed
+    led.toggle()
+    if led.is_lit :
+        print("Led turned on >>>")
+    else :
+        print("Led turned off <<<")    
+def loop():
+    #Button detect
+    button.when_pressed = onButtonPressed
+    while True:
+        time.sleep(1)
+def destroy():
+    led.close()
+    button.close()    
+if __name__ == '__main__':     # Program entrance
+    print ('Program is starting...')
+    try:
+        loop()
+    except KeyboardInterrupt:  # Press ctrl-c to end the program.
+        destroy()
+        print("Ending program")
